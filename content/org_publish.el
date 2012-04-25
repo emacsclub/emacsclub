022acc289716cc901240eae46289146d706ceb2c
Mishal Awadah <a.mamish@gmail.com>
2012-04-25 16:01:22 -0400

    added meeting notes to html project

--- content/org_publish.el
+++ content/org_publish.el
@@ -1,20 +1,35 @@
 (defconst home (file-name-directory (or load-file-name buffer-file-name)))
- 
+
 (require 'org-publish)
 (setq org-publish-project-alist
       `(
         ;; add all the components here
         ;; *notes* - publishes org files to html
-        ("org-notes"
+        ("main-page"
          :base-directory ,(concat home "org/")
          :base-extension "org"  ; Filename suffix without dot
          :publishing-directory ,(concat home "html/")
-         :recursive t           ; includes subdirectories
+         :recursive t           ; DONT include subdirectories
          :publishing-function org-publish-org-to-html
          :headline-levels 4             ; Just the default for this project.
+         :style "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/stylesheet.css\" />"
          :auto-preamble t
          :auto-sitemap t                ; generate automagically
-         :sitemap-title "Sitemap"
+         :sitemap-title "Main Sitemap"
+         )
+
+        ;; The meeting notes to be published
+        ("meeting-notes"
+         :base-directory ,(concat home "org/meeting-notes/")
+         :base-extension "org"
+         :publishing-directory ,(concat home "html/meeting-notes/")
+         :recursive nil
+         :publishing-function org-publish-org-to-html
+         :headline-levels 4
+         :style "<link rel=\"stylesheet\" type=\"text/css\" href=\"../../css/stylesheet.css\" />"
+         :auto-preamble t
+         :auto-sitemap t
+         :sitemap-title "Meeting Notes Directory"
          )
 
         ;; *static* - copies files to directories
@@ -27,6 +42,6 @@
          )
 
         ;; *publish* with M-x org-publish-project RET emacsclub RET
-        ("emacsclub" :components ("org-notes" "org-static"))
+        ("emacsclub" :components ("main-page" "meeting-notes" "org-static"))
         ))
 
